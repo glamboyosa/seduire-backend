983211e6f5571887b9692e0f45f6799f60bfe861
unknown <ogbemudiatimothy@gmail.com>
2020-03-14 02:30:42 +0100

    modified index

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({ credentials: true, origin: true }));
 app.options('*', cors());
 app.use(AuthMiddleware);
 app.use(
